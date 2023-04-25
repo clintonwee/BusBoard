684fe03a8695a90c74cd05f50259f38bfa301c87
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-25 14:51:11 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>The current time is {currentTime}.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
