050e349efebc9027f3ae283d265984223dffb571
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-25 15:16:11 +0100

    Get latitude and longitude from postcode

--- api/api.py
+++ api/api.py
@@ -12,5 +12,13 @@ def get_current_time():
 def get_bus(postcode):
     postcode_url = "https://api.postcodes.io/postcodes/{}".format(postcode)
     response = requests.get(postcode_url)
+    result = response.json()
+    
+    
+    longitude = result['result']['longitude']
+    latitude = result['result']['latitude']
+    
+    print("Longitude : {}".format(longitude))
+    print("Latitude : {}".format(latitude))
     print(response)
     return {'response': response.json()}
