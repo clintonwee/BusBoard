8d0578cffa86bc59210b664ef8063f22a442d8ef
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-25 16:45:29 +0100

    Refactored api, separated functions into separate utils file

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import time
 import requests
 from flask import Flask
+from utils import get_coordinates, get_stop_points, get_buses
 
 app = Flask(__name__)
 
@@ -10,42 +11,11 @@ def get_current_time():
 
 @app.route('/bus/<postcode>')
 def get_bus(postcode):
-    postcode_url = "https://api.postcodes.io/postcodes/{}".format(postcode)
-    response = requests.get(postcode_url)
-    result = response.json()
+
+    longitude, latitude = get_coordinates(postcode)
     
+    stop_points = get_stop_points(longitude, latitude)
     
-    longitude = result['result']['longitude']
-    latitude = result['result']['latitude']
-    
-    print("Longitude : {}".format(longitude))
-    print("Latitude : {}".format(latitude))
-    
-    stopid_url = "https://api.tfl.gov.uk/StopPoint/?lat={}&lon={}&stopTypes=NaptanPublicBusCoachTram".format(latitude, longitude)
-    response = requests.get(stopid_url)
-    result = response.json()
-    
-    #distance, name of road (commonName), name of stop (indicator), stopcode (naptanId)
-    results = []
-    for stopPoint in result['stopPoints'][:2]:
-        stopid = stopPoint['id']
-        bus_url = 'https://api.tfl.gov.uk/StopPoint/{}/Arrivals'.format(stopid)
-        response = requests.get(bus_url)
-        next_buses = [
-                {
-                    'eta': bus['timeToStation'],
-                    'destination': bus['destinationName'],
-                    'busName': bus['lineName']
-                } for bus in response.json()]
-        
-        stop_info = {
-            "stopName": stopPoint['indicator'],
-            "roadName": stopPoint['commonName'],
-            "buses": next_buses
-        }
-        
-        results.append(stop_info)
-        # print(stopPoint['commonName'])
-    
+    results = get_buses(stop_points)
     
     return {'response': results, 'status': 'success'}
---
+++ api/utils.py
@@ -0,0 +1,41 @@
+import requests
+
+def get_coordinates(postcode):
+    postcode_url = "https://api.postcodes.io/postcodes/{}".format(postcode)
+    response = requests.get(postcode_url)
+    result = response.json()
+    
+    longitude = result['result']['longitude']
+    latitude = result['result']['latitude']
+    
+    return longitude, latitude
+
+def get_stop_points(longitude, latitude):
+    stopid_url = "https://api.tfl.gov.uk/StopPoint/?lat={}&lon={}&stopTypes=NaptanPublicBusCoachTram".format(latitude, longitude)
+    response = requests.get(stopid_url)
+    result = response.json()
+    return result['stopPoints'][:2]
+
+def get_buses(stop_points):
+    results = []
+    for stopPoint in stop_points:
+        stopid = stopPoint['id']
+        bus_url = 'https://api.tfl.gov.uk/StopPoint/{}/Arrivals'.format(stopid)
+        response = requests.get(bus_url)
+        next_buses = [
+                {
+                    'eta': bus['timeToStation'],
+                    'destination': bus['destinationName'],
+                    'busName': bus['lineName']
+                } for bus in response.json()]
+        
+        stop_info = {
+            "stopName": stopPoint['indicator'],
+            "roadName": stopPoint['commonName'],
+            "buses": next_buses
+        }
+        
+        results.append(stop_info)
+    
+    return results
+    
