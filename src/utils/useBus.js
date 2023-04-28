884fbd6f4639e97153f8f887b923e2853159575c
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-28 11:54:50 +0100

    Renamed variables to be more specific

--- src/utils/useBus.js
+++ src/utils/useBus.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function useBus(postcode) {
   const [hasRun, setHasRun] = useState(false);
 
-  const fetcher = async () => {
+  const busFetcher = async () => {
     if (postcode.length === 0) {
       return [];
     }
@@ -12,14 +12,13 @@ function useBus(postcode) {
       const res = await fetch(`/bus/${postcode}`);
       const data = await res.json();
       setHasRun(true);
-      console.log(data);
       return data.response;
     } catch (e) {
       throw new Error("Invalid Postcode");
     }
   };
 
-  const { data, error, isLoading } = useSWR(`/bus/${postcode}`, fetcher, {
+  const { data, error, isLoading } = useSWR(`/bus/${postcode}`, busFetcher, {
     fallbackData: [],
     refreshInterval: 10000,
   });
